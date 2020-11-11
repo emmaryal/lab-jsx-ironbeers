e0fe5c6b50db477ce91e2953e45cca45afe9884a
Emma Craig <emma.ryal@gmail.com>
2020-11-11 20:35:42 +0100

    added keys

--- views/RandomBeer.jsx
+++ views/RandomBeer.jsx
@@ -6,10 +6,10 @@ function RandomBeer(props) {
     <Layout>
       {
         <div>
-          {props.beersFromApi.map(beerObj => {
+          {props.beersFromApi.map((beerObj, i) => {
             return (
               <div>
-                <img src={beerObj.image_url} width="200" alt="" />
+                <img key = {i} src={beerObj.image_url} width="200" alt="" />
                 <h3>{beerObj.name}</h3>
                 <h5>{beerObj.tagline}</h5>
                 <p>{beerObj.description}</p>
